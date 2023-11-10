6475ab5ca1ec808924d71a7188c364ddab7f8ddc
Leangeful <cuttheleash1@gmail.com>
2023-11-10 23:00:09 +0100

    update bas64 encode

--- src/lib/Base64.ts
+++ src/lib/Base64.ts
@@ -1,47 +1,39 @@
-const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+//const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+const alphabet = [
+	65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88,
+	89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114,
+	115, 116, 117, 118, 119, 120, 121, 122, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 43, 47
+];
 const pad = 61;
+const mask = 63;
 
 export const Base64 = {
-	encode(arr: Uint8Array): Uint8Array {
-		const mask = 63;
-		const len = arr.length;
-		const resLen = Math.ceil(len / 3) * 4;
-		const result = new Uint8Array(resLen);
+	encode(inArr: Uint8Array): Uint8Array {
+		const rest = inArr.length % 3;
+		const arr = new Uint8Array(inArr.length + (rest ? 3 - rest : 0));
+		arr.set(inArr);
 
-		let resIdx = 0;
-		let i = 0;
+		const padding = rest ? (rest == 2 ? [pad] : [pad, pad]) : [];
 
-		for (; i < len - 2; ) {
-			let bits = arr[i] << 16;
-			bits += arr[i + 1] << 8;
-			bits += arr[i + 2];
+		const result = new Uint8Array((arr.length / 3) * 4);
 
-			result[resIdx] = alphabet[(bits >> 18) & mask].charCodeAt(0);
-			result[resIdx + 1] = alphabet[(bits >> 12) & mask].charCodeAt(0);
-			result[resIdx + 2] = alphabet[(bits >> 6) & mask].charCodeAt(0);
-			result[resIdx + 3] = alphabet[bits & mask].charCodeAt(0);
+		for (let i = 0; i < arr.length - 2; i += 3) {
+			const bits = (arr[i] << 16) + (arr[i + 1] << 8) + arr[i + 2];
 
-			resIdx += 4;
-			i += 3;
+			const resIdx = (i / 3) * 4;
+			result.set(
+				[
+					alphabet[(bits >>> 18) & mask],
+					alphabet[(bits >>> 12) & mask],
+					alphabet[(bits >>> 6) & mask],
+					alphabet[bits & mask]
+				],
+				resIdx
+			);
 		}
 
-		const rest = len % 3;
+		result.set(padding, result.length - padding.length);
 
-		if (rest) {
-			let bits = arr[i] << 16;
-			bits += i + 1 < len ? arr[i + 1] << 8 : 0;
-
-			result[resIdx] = alphabet[(bits >> 18) & mask].charCodeAt(0);
-			result[resIdx + 1] = alphabet[(bits >> 12) & mask].charCodeAt(0);
-
-			if (rest == 2) {
-				result[resIdx + 2] = alphabet[(bits >> 6) & mask].charCodeAt(0);
-			} else {
-				result[resIdx + 2] = pad;
-			}
-
-			result[resIdx + 3] = pad;
-		}
 		return result;
 	}
 };
